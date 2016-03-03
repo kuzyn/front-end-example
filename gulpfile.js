54a3ead244a977178bacd42de60639a04af23422
kuzyn <hello@samuelcousin.com>
2016-03-03 12:44:54 -0500

    chore: npm & a tiny bit more tooling

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 //////////////////////////////////////
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
 var bs = require('browser-sync').create();
 
 // initiating our browser-sync task
@@ -15,9 +17,18 @@ gulp.task('browser-sync', function() {
     });
 });
 
+// our concat task
+gulp.task('scripts', function() {
+  return gulp.src(['./app/**/*.js'])
+    .pipe(concat('app.concat.js'))
+    .pipe(notify("Updated <%= file.relative %>"))
+    .pipe(gulp.dest('./'));
+});
+
 // our default task
 gulp.task('default', ['browser-sync'], function () {
 	gulp.watch('./index.html', bs.reload);
-	gulp.watch('./app/**/*.js', bs.reload);
+	gulp.watch(['./app/**/*.js'], ['scripts']);
+	gulp.watch('./app.concat.js', bs.reload);
 	gulp.watch('./assets/**/*.*', bs.reload);
 });
